7c3176c50d835fc9f2a037004af844d77b6f2592
Zane Billings <wz.billings@gmail.com>
2024-03-30 17:24:42 -0400

    start data processing script

---
+++ Project/R/data-processing.R
@@ -0,0 +1,13 @@
+###
+# Sequence data cleaning
+# Zane Billings
+# 2024-03-30
+# Read in the raw sequence data that I got from GISAID and UniProt, and make
+# sure everything is consistently formatted.
+###
+
+# ---- Setup ----
+
+
+
+# ---- END OF FILE ----
